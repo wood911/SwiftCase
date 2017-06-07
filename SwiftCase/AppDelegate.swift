fb375e5831c14793b3858bac07c6cb302957d8d8
woodpower <powerwtf@live.com>
2017-06-07 13:01:37 +0800

    debug

--- SwiftCase/AppDelegate.swift
+++ SwiftCase/AppDelegate.swift
@@ -17,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // debug layer -> Private API
+//        let overlayClass = NSClassFromString("UIDebuggingInformationOverlay") as? UIWindow.Type
+//        _ = overlayClass?.perform(NSSelectorFromString("prepareDebuggingOverlay"))
         return true
     }
 
